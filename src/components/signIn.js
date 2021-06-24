f6884f00ac6a4f13e409f747fe160f91f1f46261
斉藤雄飛 <yuhi.iphone819@gmail.com>
2021-06-24 15:32:23 +0900

    save name by enter key

--- src/components/signIn.js
+++ src/components/signIn.js
@@ -69,6 +69,13 @@ export default function SignIn({ setName }) {
             name="name"
             autoFocus
             onChange={(e) => setString(e.target.value)}
+            onKeyDown={(e) => {
+              if( e.key === 'Enter') {
+                setName(e.target.value);
+                e.preventDefault();
+
+              }
+            }}
           />
           
           
