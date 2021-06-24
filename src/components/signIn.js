58ff4608960c45f22c7124e507b0ddec1e5b8661
斉藤雄飛 <yuhi.iphone819@gmail.com>
2021-06-24 15:04:13 +0900

    setName by cliking button

--- src/components/signIn.js
+++ src/components/signIn.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -41,6 +41,14 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignIn({ setName }) {
   const classes = useStyles();
+  const [disabled, setDisabled] = useState(true);
+  const [string, setString] = useState('');
+  console.log({ disabled, string});
+
+  useEffect(() => {
+    const disabled = string === ''
+     setDisabled(disabled)
+  }, [string])
 
   return (
     <Container component="main" maxWidth="xs">
@@ -60,6 +68,7 @@ export default function SignIn({ setName }) {
             label="ニックネーム"
             name="name"
             autoFocus
+            onChange={(e) => setString(e.target.value)}
           />
           
           
@@ -69,6 +78,7 @@ export default function SignIn({ setName }) {
             variant="contained"
             color="primary"
             className={classes.submit}
+            disabled={disabled}
           >
             始める
           </Button>
