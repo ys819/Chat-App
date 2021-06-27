4335cb95dd5273911966effa8a3662137eb26e25
斉藤雄飛 <yuhi.iphone819@gmail.com>
2021-06-27 19:44:18 +0900

    apply grid layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles'
+
+import MesssageInputField from './MessageInputField'
+import MessageList from './MessageList'
+
+const useStyles = makeStyles({
+    root: {
+        display: 'grid',
+        height: '100vh',
+        gridTemplateRows: '1fr auto',
+    },
+});
 
 const Main = () => {
-  return <div>Main</div>;
+    const classes = useStyles();
+
+  return <div className={classes.root}>
+      <MessageList />
+      <MesssageInputField />
+      </div>;
 
 };
 
