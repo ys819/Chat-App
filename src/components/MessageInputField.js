78019e5ba5b67b033253744050d8c9c19c839533
斉藤雄飛 <yuhi.iphone819@gmail.com>
2021-06-28 10:41:51 +0900

    generate avater

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Avatar, Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
+import { gravatarPath } from '../gravatar'
 
 const useStyles = makeStyles({
     root: {
@@ -10,13 +11,15 @@ const useStyles = makeStyles({
     },
 });
 
-const MessageInputField = () => {
+const MessageInputField = ({ name }) => {
     const classes = useStyles()
+    const avatarPath = gravatarPath(name);
+
     return (
     <div className={classes.root}>
         <Grid container>
             <Grid xs={1}>
-                <Avatar />
+                <Avatar src={avatarPath} />
             </Grid>
             <Grid xs={10}>
                 入力
