5eeeffbd97c510ff71772a4a604d84a48b2d645e
斉藤雄飛 <yuhi.iphone819@gmail.com>
2021-06-30 23:54:05 +0900

    fetch messages

--- src/firebase.js
+++ src/firebase.js
@@ -23,7 +23,7 @@ const firebaseConfig = {
 
   firebase.initializeApp(firebaseConfig);
    const database = firebase.database();
-   const messagesRef =  database.ref('messages');
+   export const messagesRef =  database.ref('messages');
 
    export const pushMessage = ({ name, text }) => {
        messagesRef.push({ name, text });
