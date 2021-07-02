f4837ac6940ad225b07f41a7cace0ad3b23d594b
斉藤雄飛 <yuhi.iphone819@gmail.com>
2021-07-02 16:12:29 +0900

    create command to generate .env.local

---
+++ .firebase-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyAs2Pmgh7WWDSyFjSwuXeGbKvBLJKnfxTc",
+    authDomain: "chat-app-33611.firebaseapp.com",
+    databaseURL: "https://chat-app-33611-default-rtdb.firebaseio.com",
+    projectId: "chat-app-33611",
+    storageBucket: "chat-app-33611.appspot.com",
+    messagingSenderId: "12836222273",
+    appId: "1:12836222273:web:19c8fcffb57fb812655dc0"
+  }
